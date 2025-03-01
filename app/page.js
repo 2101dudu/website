6339c5726367fd359fa0bbc162f2a504c2c23440
Eduardo Faria <eduardo21faria@gmail.com>
2025-03-01 18:12:26 +0000

    feat: first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import SplitText from "./SplitText";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-1/2 h-1/4 flex-row">
+        <h1 className="text-7xl font-extrabold text-center">This website is still being compiled.</h1>
+        <h1 className="text-3xl text-neutral-500 italic text-center pt-7">Please check back soon.</h1>
+      </div>
+    </div>
+  );
+}
