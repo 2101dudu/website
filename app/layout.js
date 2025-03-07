0f7196213cdaa5a28c8b0e243d416de85c516631
Eduardo Faria <eduardo21faria@gmail.com>
2025-03-07 23:12:20 +0000

    feat: add vercel tracking

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const jetBrains = JetBrains_Mono({
   variable: "--font-jet-brains",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         className={`${jetBrains.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
