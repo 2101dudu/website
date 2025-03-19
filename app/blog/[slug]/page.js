7be8ceea549092c67033ab4ba72b04821a83eefa
Eduardo Faria <eduardo21faria@gmail.com>
2025-03-19 23:04:43 +0000

    feat: first blog post almost done

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,33 +1,18 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
-import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github.css";
-
-export default async function BlogEntry({ params }) {
+export default async function Page({ params }) {
   const { slug } = await params;
-  const filePath = path.join(process.cwd(), "posts", `${slug}.md`);
-  const fileContents = fs.readFileSync(filePath, "utf8");
-
-  const { data, content } = matter(fileContents);
-
-  const processedContent = await remark()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeHighlight)
-    .use(rehypeStringify)
-    .process(content);
-
-  const contentHtml = processedContent.toString();
+  const { default: Post } = await import(`../../../content/${slug}.mdx`);
 
   return (
-    <div className="max-w-screen-lg mx-auto p-4">
-      <h1 className="text-4xl font-bold">{data.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+    <div className="mt-50 w-screen h-screen flex flex-col justify-start items-center gap-20">
+      <div className="max-w-screen-lg flex flex-col justify-around items-start">
+        <Post />
+      </div>
     </div>
   );
 }
+
+export function generateStaticParams() {
+  return [{ slug: "welcome" }, { slug: "blockchain" }, { slug: "homeserver" }];
+}
+
+export const dynamicParams = false;
